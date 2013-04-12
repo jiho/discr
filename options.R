776a880a2e459835dec5a5a7bf8adede3f1bde92
Jean-Olivier Irisson <irisson@normalesup.org>
2013-04-12 17:30:35 +0200

    Add comments

--- options.R
+++ options.R
@@ -1,38 +1,59 @@
-disc_read_options <- function(file="disc.conf", quiet=TRUE) {
+#
+#      Handle disc-specific options
+#
+#  (c) Copyright 2013 Jean-Olivier Irisson
+#      GNU General Public License v3
+#
+#--------------------------------------------------------------------------
+
+disc_read_options <- function(file="disc.conf", verbose=FALSE) {
+  #
+  # Read disc-specific options from the config file
+  #
+  # file    config file, must contain the definition of an R list with options
+  # verbose if TRUE, show options on the console
+
   # read disc specific options
   if (file.exists(file)) {
-    opDisc <- dget(file)
+    opts.disc <- dget(file)
   } else {
-    opDisc <- list()
+    opts.disc <- list()
   }
 
-  if (!quiet) {
+  # print them if requested
+  if (verbose) {
     message("DISC options")
-    print(opDisc)
+    print(opts.disc)
   }
 
-  # store them as global R options to avoid passing them around in functions
+  # store them as global R options to avoid passing them around in functions or using a global variable
   op <- options()
-  toset <- !(names(opDisc) %in% names(op))
-  if(any(toset)) options(opDisc[toset])
+  toset <- !(names(opts.disc) %in% names(op))
+  if(any(toset)) options(opts.disc[toset])
 
-  return(opDisc)
+  return(opts.disc)
 }
 
-disc_write_options <- function(op, file="disc.conf") {
+disc_write_options <- function(opts, file="disc.conf") {
+  #
+  # Write disc-specific options in a configuration file
+  #
+  # opts  disc options
+  # file  config file
+
   # # transform options list into a character string representation
-  # opStr <- deparse(op, control=NULL, width.cutoff=500)
-  # 
+  # opStr <- deparse(opts, control=NULL, width.cutoff=500)
+  #
   # # reformat it to be human readable
   # suppressPackageStartupMessages(require("stringr", quietly=TRUE))
   # opStr <- str_c(opStr, collapse="")
   # opStr <- str_replace(opStr, ")$", "\n)\n")
   # opStr <- str_replace_all(opStr, "disc", "\n\tdisc")
-  # 
+  #
   # # write it in the options file
   # cat(opStr, file=file)
 
-  dput(op, file=file)
-  
+  dput(opts, file=file)
+
   return(invisible(file))
 }
