728ba9937273b53a195990a90ec7f491926c6a4b
Jean-Olivier Irisson <irisson@normalesup.org>
2014-03-11 13:36:27 +0100

    Reimplement status information on given directory

---
+++ R/file_names.R
@@ -0,0 +1,22 @@
+.files <- list(
+  # raw data file names
+  
+  # processed file names
+  video = "video_hifi.mov",
+  pictures = "pics",
+  
+  digital.compass = "compass_log.csv",
+  manual.compass = "compass_track.txt",
+  manual.compass.coord = "coord_compass.txt",
+  
+  ctd = "ctd_log.csv",
+  gps = "gps_log.csv",
+  light = "hobo_log.csv",
+
+  aquarium.coord = "coord_aquarium.csv",
+  
+  tracks = "larvae_tracks.txt",
+  corrected.tracks = "tracks.csv",
+
+  stats = "stats.csv"
+)
--- R/status.R
+++ R/status.R
@@ -1,66 +1,75 @@
-#
-#      Give status information about the deployments directory
-#
-#  (c) Copyright 2013 Jean-Olivier Irisson
-#      GNU General Public License v3
-#
-#--------------------------------------------------------------------------
+#' Status of the working directory
+#'
+#' Give information about deployments in the working directory (number of pictures, metadata files, data files, etc.)
+#'
+#' @param dir path to the working directory. Read from the option \code{disc.wd} by default.
+#'
+#' @seealso \code{\link{disc_setwd}} to set the working directory option.
+#' @export
+#' @importFrom plyr ldply
+disc_status <- function(dir=getOption("disc.wd")) {
 
+  if (!file.exists(dir)) {
+    stop("Cannot find ", dir)
+  }
+  message(dir)
 
-disc_status <- function(dir="deployments") {
+  # list all deployments
+  deployments <- list.dirs(dir, full.names=FALSE, recursive=FALSE)
 
-  suppressPackageStartupMessages(require("plyr", quietly=TRUE))
-  
-  deploys <- sort(as.numeric(list.files(dir, pattern="[0-9]*")))
-  
-  d <- adply(deploys, 1, function(i) {
-    
-  	# test the existence of video
-    video <- file.exists(make_path(dir, i, "video_hifi.mov"))
+  d <- ldply(deployments, function(i) {
 
-  	# count images
-    pics <- list.files(make_path(dir, i, "pics"), pattern=glob2rx("*.jpg"))
+    # test the existence of video
+    video <- file.exists(make_path(dir, i, .files$video))
+
+    # count images
+    pics <- list.files(make_path(dir, i, .files$pictures), pattern=glob2rx("*.jpg"))
     nbPics <- length(pics)
 
-  	# test the existence of data files
-    autoCompass <- file.exists(make_path(dir, i, "compass_log.csv"))
-    manuCompass <- file.exists(make_path(dir, i, "compass_track.txt")) & file.exists(make_path(dir, i, "coord_compass.txt"))
-    if (autoCompass) {
-      compass <- "auto"
-    } else if (manuCompass) {
-      compass <- "man"
+    # test the existence of data files
+    digitalCompass <- file.exists(make_path(dir, i, .files$digital.compass))
+    manualCompass  <- file.exists(make_path(dir, i, .files$manual.compass)) & file.exists(make_path(dir, i, .files$manual.compass.coord))
+    if (digitalCompass) {
+      compass <- "digi"
+    } else if (manualCompass) {
+      compass <- "manu"
     } else {
       compass <- FALSE
     }
-    ctd <- file.exists(make_path(dir, i, "ctd_log.csv"))
-    gps <- file.exists(make_path(dir, i, "gps_log.csv"))
+    ctd <- file.exists(make_path(dir, i, .files$ctd))
+    gps <- file.exists(make_path(dir, i, .files$gps))
+    light <- file.exists(make_path(dir, i, .files$light))
 
-  	# test the existence of data files
-    calib <- file.exists(make_path(dir, i, "coord_aquarium.csv"))
-    rawTracks <- file.exists(make_path(dir, i, "larvae_tracks.txt"))
-    correctedTracks <- file.exists(make_path(dir, i, "tracks.csv"))
-    stats <- file.exists(make_path(dir, i, "stats.csv"))
-    if (stats) {
-      status <- TRUE
-    } else if (correctedTracks) {
-      status <- "correction done"
-    } else if (rawTracks) {
-      status <- "tracking done"
-    } else if (calib) {
-      status <- "calibration done"
-    } else {
-      status <- FALSE
-    }
+    # test the existence of data files
+    calib   <- file.exists(make_path(dir, i, .files$coord.aquarium))
+    track   <- file.exists(make_path(dir, i, .files$tracks))
+    correct <- file.exists(make_path(dir, i, .files$corrected.tracks))
+    stats   <- file.exists(make_path(dir, i, .files$stats))
+    # if (stats) {
+    #   status <- TRUE
+    # } else if (correctedTracks) {
+    #   status <- "correction done"
+    # } else if (rawTracks) {
+    #   status <- "tracking done"
+    # } else if (calib) {
+    #   status <- "calibration done"
+    # } else {
+    #   status <- FALSE
+    # }
 
-    d <- data.frame(video, pics=nbPics, compass, ctd, gps, status, stringsAsFactors=FALSE)
+    d <- data.frame(video, pics=nbPics, compass, ctd, gps, light, calib, track, correct, stats, stringsAsFactors=FALSE)
   })
-  d <- d[,-1]
-  row.names(d) <- deploys
+  row.names(d) <- deployments
+
+  # remove columns that are entirely empty/FALSE
+  # those are usually non-existent sensors
+  empty <- laply(d, function(x) { all(x == FALSE) } )
+  d <- d[, ! empty ]
 
   class(d) <- c("disc_status", "data.frame")
-  
+
   return(d)
-  
+
 #   # when the corrected track is available, use the times there
 #   if [[ -e $work/$i/tracks.csv ]]; then
 #     # with R read the corrected tracks and compute average time difference
@@ -72,7 +81,7 @@ disc_status <- function(dir="deployments") {
 #       # compute the average time difference
 #       cat(sprintf("%4i",round(mean(diff(imgTimes)))))
 # EOF
-# 
+#
 #   # otherwise fall back on the uncorrected track
 #   # but in this case times have to be extracted from the image files
 #   elif [[ -e $work/$i/larvae_track.txt ]]; then
@@ -80,10 +89,10 @@ disc_status <- function(dir="deployments") {
 #     # select the first 3 lines, remove the header, and select 4th column
 #     imgNbs=$(head -n 3 $work/$i/larvae_track.txt | sed \1d | awk -F "\t" {'print $4'})
 #     # this selects only the first 2 images but using more slows things down when exiftool reads the creation date
-# 
+#
 #     # re-construct the full paths to images
 #     images=$(echo $imgNbs | awk '{OFS=""; print pics,$1,".jpg ",pics,$2,".jpg"}' pics="${work}/${i}/pics/")
-# 
+#
 #     # compute the time difference in R
 #     R -q --slave << EOF
 #       # get images capture times with exiftool
@@ -92,7 +101,7 @@ disc_status <- function(dir="deployments") {
 #       imgTimes = as.POSIXct(imgTimes,format="%Y:%m:%d %H:%M:%S")
 #       cat(sprintf("%4i",as.numeric(diff(imgTimes))))
 # EOF
-	
+
 #   # display subsample interval
 #   if [[ -e $work/$i/stats.csv ]]; then
 #     R -q --slave << EOF
@@ -108,15 +117,19 @@ disc_status <- function(dir="deployments") {
 #   else
 #     echo -n "    "
 #   fi
-# 
+#
 #   echo ""
- 
+
 }
 
+#' @rdname disc_status
+#' @export
+dstatus <- disc_status
 
 print.disc_status <- function(x) {
-  x[x==FALSE] <- "-"
-  x[x==TRUE] <- "✔"
+  x[x==FALSE] <- "x"
+  # x[x==TRUE] <- "✔"
+  x[x==TRUE] <- "."
   print.data.frame(x)
   return(invisible(x))
 }
