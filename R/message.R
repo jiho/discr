6f4f8c6810a54570e06c29bc2f96059f846c42e4
Jean-Olivier Irisson <irisson@normalesup.org>
2014-03-13 16:15:24 +0100

    Add a custom message handler which allows indentation

---
+++ R/message.R
@@ -0,0 +1,33 @@
+#' Write a progress message on the console
+#'
+#' This is similar to \code{message} except it will add nice indentation and won't be bold
+#'
+#' @param ... message components passed to \code{cat}
+#' @importFrom stringr str_c
+#' @importFrom stringr str_trim
+#' @importFrom stringr str_sub
+#' @importFrom stringr str_length
+#' @keywords internal
+disc_message <- function(...) {
+  # concatenate the message bits
+  message <- str_c(..., sep=" ")
+  
+  # compute how many lines it will take
+  nCharaters <- str_length(message)
+  indentLength <- 2
+  lineLength <- 71 - indentLength
+  nLines <- nCharaters %/% lineLength
+  
+  # write every line
+  for (i in 1:nLines) {
+    thisLine <- str_sub(message, (i-1)*lineLength+1, i*lineLength)
+    thisLine <- str_trim(thisLine)
+    cat(rep(" ", indentLength), thisLine, "\n", sep="")
+  }
+  
+  return(invisible(NULL))
+}
+
+#' @rdname disc_message
+#' @keywords internal
+dmess <- disc_message
