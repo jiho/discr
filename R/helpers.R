999a37fb027c95df30ea68ea3cffdf59acd730d7
Jean-Olivier Irisson <irisson@normalesup.org>
2014-03-13 11:44:39 +0100

    Revert "Simplify the function to create paths"
    This reverts commit 7d4e97c024fbcb1caf1821565661efa1aeefbe98.
    
    This simplification did not work to expand vector arguments

--- R/helpers.R
+++ R/helpers.R
@@ -8,7 +8,13 @@ make_path <- function(...) {
   args <- list(...)
 
   # paste them together
-  path <- paste(args, collapse="/")
+  nArgs <- length(args)
+  path <- args[[1]]
+  if (nArgs > 1) {
+    for (i in 2:nArgs) {
+      path <- paste(path, args[[i]], sep="/")
+    }
+  }
 
   # make sure the path is valid and clean (perform path expansion, remove repeated path separators, etc.)
   path <- normalizePath(path, winslash="/", mustWork=FALSE)
