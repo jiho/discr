4ed946134260141d7c4950700b55727825f32ff9
Jean-Olivier Irisson <irisson@normalesup.org>
2014-05-27 23:13:27 +0200

    Cleanup calibration step
    Better error messages
    Pre-process ImageJ output

--- R/calibrate.R
+++ R/calibrate.R
@@ -1,10 +1,13 @@
 #' Calibrate aquarium position
 #'
-#' @param dir path the to the deployment directory
+#' @param dir path to the deployment directory
+#' @param ... passthrough argument
 #'
 #' @export
 #' @importFrom stringr str_c
 disc_calibrate <- function(dir, ...) {
+
+	disc_message("calibration")
 
   picsDir <- make_path(dir, .files$pictures)
   if (! file.exists(picsDir)) {
@@ -20,8 +23,7 @@ disc_calibrate <- function(dir, ...) {
   aquariumCoordFile <- tempfile()
   aquariumBoundingBoxFile <- tempfile()
 
-	message("CALIBRATION")
-	message("Opening first image for calibration")
+  # message("Opening first image for calibration")
 
   # prepare java command
   command <- str_c(
@@ -31,8 +33,7 @@ disc_calibrate <- function(dir, ...) {
     " makeOval(", getOption("disc.aquarium"), ");",
     " setTool('oval');",
     " waitForUser('Aquarium selection',",
-    " 'If necessary, alter the selection to fit the aquarium better.\\n",
-    " Press OK when you are done');",
+    " 'If necessary, alter the selection to fit the aquarium better.\\nPress OK when you are done');",
     " run('Set Measurements...', '  centroid perimeter invert redirect=None decimal=3');",
     " run('Measure');",
     " saveAs('Measurements', '", aquariumCoordFile, "');",
@@ -45,33 +46,26 @@ disc_calibrate <- function(dir, ...) {
 
   # run the command and check for success
   status <- system(command)
-  check_status(status, message="Abort calibration")
+  check_status(status, message="Error running ImageJ. Aborting")
 
-  # save aquarium center and perimenter
+  # save aquarium centre and perimentre
   if (file.exists(aquariumCoordFile)) {
-    file.copy(aquariumCoordFile, make_path(dir, .files$aquarium.coord))
+    d <- read.delim(aquariumCoordFile, row.names=1, col.names=c("nb", "x", "y", "perim"))
+    write.csv(d, file=make_path(dir, .files$aquarium.coord), row.names=FALSE)
     file.remove(aquariumCoordFile)
   } else {
-    stop("Abort calibration")
+    stop("Cannot find aquarium coordinates file: ", aquariumCoordFile, ". Aborting")
   }
 
-  # save bouding box of aquarium in setting
+  # save bounding box of aquarium in settings
   if (file.exists(aquariumBoundingBoxFile)) {
     bounds <- read.delim(aquariumBoundingBoxFile, row.names=1)
     bounds <- str_c(as.character(bounds[1,]), collapse=",")
     disc_conf(aquarium=bounds)
     file.remove(aquariumBoundingBoxFile)
   } else {
-    stop("Abort calibration")
+    stop("Cannot find aquarium bounding box file: ", aquariumBoundingBoxFile, ". Aborting")
   }
 
   return(invisible(status))
 }
-
-#' @rdname disc_calibrate
-#' @export
-dcalibrate <- disc_calibrate
-
-#' @rdname disc_calibrate
-#' @export
-dcalib <- disc_calibrate
